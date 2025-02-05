c268eb9b42ebb8ffe544a80df4543e1765ec8a13
nuthakkinaredra <nuthakkinarendra92@gmail.com>
2025-02-05 14:31:47 +0530

    changed

--- jenkins-server-terraform/07_iam-instance-profile.tf
+++ jenkins-server-terraform/07_iam-instance-profile.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_instance_profile" "instance-profile" {
-  name = "jenkins-server-instance-profile"
+  name = "jenkins-server-instance-profilenari"
   role = aws_iam_role.iam-role.name
 }
